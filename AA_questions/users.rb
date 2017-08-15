929a7654c8a7732bf54320e6ea9714896eb02af2
App Academy Student <student@appacademy.io>
2017-08-15 15:44:25 -0700

    write users.rb

--- AA_questions/users.rb
+++ AA_questions/users.rb
@@ -1,5 +1,47 @@
-require_relative "questions_db_manager.rb"
+require_relative 'questions_db_manager'
 
 class User
-  
+
+  def self.find_by_id(id)
+    user = Questions_DB_Manager.instance.execute(<<-SQL, id)
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      id = ?
+    SQL
+    User.new(user.first)
+  end
+
+  def self.find_by_name(fname, lname)
+    user = Questions_DB_Manager.instance.execute(<<-SQL, fname, lname)
+    SELECT
+      *
+    FROM
+      users
+    WHERE
+      fname = ? AND lname = ?
+    SQL
+    User.new(user.first)
+  end
+
+  def self.all
+    entries = Questions_DB_Manager.instance.execute(<<-SQL)
+    SELECT
+      *
+    FROM
+      users
+    SQL
+
+    entries.map { |entry| User.new(entry) }
+  end
+
+  attr_accessor :fname, :lname, :id
+
+  def initialize(options)
+    @fname = options['fname']
+    @lname = options['lname']
+    @id = options['id']
+  end
 end
