ba165c20b187c4c2f20b3322945e27bd3d22496c
App Academy Student <student@appacademy.io>
2017-08-15 16:28:55 -0700

    write question_lenth.rb

--- AA_questions/question_likes.rb
+++ AA_questions/question_likes.rb
@@ -1,1 +1,31 @@
 class QuestionLike
+  def self.find_by_author_id(author_id)
+    results = QuestionsDBManager.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM
+        question_likes
+      WHERE
+        author_id = ?
+    SQL
+    results.map { |entry| QuestionLike.new(entry) }
+  end
+
+  def self.find_by_question_id(question_id)
+    results = QuestionsDBManager.instance.execute(<<-SQL, question_id)
+      SELECT
+        *
+      FROM
+        question_likes
+      WHERE
+        question_id = ?
+    SQL
+    results.map { |entry| QuestionLike.new(entry) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @author_id = options['author_id']
+    @question_id = options['question_id']
+  end
+end
